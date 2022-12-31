817eb7644b17a28d4ac70546d754d0530646f372
최민한 <chmh0411@gmail.com>
2023-01-01 02:27:17 +0900

    add: random joke function

--- cmd/random.go
+++ cmd/random.go
@@ -4,9 +4,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // randomCmd represents the random command
@@ -15,20 +18,54 @@ var randomCmd = &cobra.Command{
 	Short: "Get a random dad joke",
 	Long:  `This command fetches a random dad joke from the icanhazdadjoke api`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("random called")
+		getRandomJoke()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(randomCmd)
+}
 
-	// Here you will define your flags and configuration settings.
+type Joke struct {
+	ID     string `json:"id"`
+	Joke   string `json:"joke"`
+	Status int    `json:"status"`
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+func getRandomJoke() {
+	url := "https://icanhazdadjoke.com/"
+	responseBytes := getJokeData(url)
+	joke := Joke{}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if err := json.Unmarshal(responseBytes, &joke); err != nil {
+		log.Print("Could not unmarshal response %v", err)
+	}
+
+	fmt.Println(string(joke.Joke))
+}
+
+func getJokeData(baseAPI string) []byte {
+	request, err := http.NewRequest(
+		http.MethodGet,
+		baseAPI,
+		nil,
+	)
+	if err != nil {
+		log.Print("Could not request a dadjoke - %v", err)
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("User-Agent", "Dadjoke CLI (github/minhaaan/cobra-example")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		log.Print("Could not make a request - %v", err)
+	}
+
+	responseBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Print("Could not read response body %v", err)
+	}
+
+	return responseBytes
 }
